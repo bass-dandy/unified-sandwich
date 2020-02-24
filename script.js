d1a5ab664e559da019e21a5fd96fef52803824b0
bass-dandy <ctdinh93@gmail.com>
2020-02-24 16:04:40 -0500

    initial push

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+	for (let i = 0; i < entries.length; i++) {
+		if (entries[i].isIntersecting) {
+			const tgt = entries[i].target;
+			tgt.classList.add('in');
+			observer.unobserve(tgt);
+		}
+	}
+}, {threshold: [0.75]});
+
+const sections = document.getElementsByTagName('section');
+
+for (let i = 0; i < sections.length; i++) {
+	observer.observe(sections[i]);
+}
